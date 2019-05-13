8cce3bd61aeb084ca598ea341934a10140f8fbee
broomspun1208 <admin@angelsolution.net>
2019-05-13 01:51:30 -0600

    user paramerter validation for user register

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator/check');
 
-//@route GET api/users/test
+//load User model
+const User = require('../../models/User');
+
+//@route POST api/users/register
 //@desc Tests users route
 //@access Public
-router.get('/test', (req, res)=>res.json({msg: "Users Works"}));
+router.post('/register',[
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+
+], (req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: 'errors'})
+    }
+
+    res.send('User route');
+});
 
 module.exports = router;
