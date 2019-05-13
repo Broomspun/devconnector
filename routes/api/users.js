f6206d0b8a63644f764af95adf6985e2883287d0
broomspun1208 <admin@angelsolution.net>
2019-05-13 02:23:54 -0600

    implement user register by email/password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
 const {check, validationResult} = require('express-validator/check');
 
 //load User model
@@ -20,8 +23,36 @@ router.post('/register',[
     if(!errors.isEmpty()){
         return res.status(400).json({errors: 'errors'})
     }
+    User.findOne({email: req.body.email})
+        .then(user=>{
+            if(user){
+                return res.status(400).json({email: 'Email already exists'})
+            }
+            else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', //Size
+                    r: 'pg', //Rating,
+                    d: 'mm', //Default
+                });
 
-    res.send('User route');
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar: avatar,
+                    password: req.body.password
+                });
+
+                bcrypt.genSalt(10, (err, salt)=>{
+                    bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user=>res.json(user))
+                            .catch(err=>console.log(err))
+                    })
+                })
+            }
+        });
 });
 
 module.exports = router;
